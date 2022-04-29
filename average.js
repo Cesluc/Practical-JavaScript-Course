89025c21c9af7be4f9127b46b1d88878db6c3125
Cesar E Lucumi A <Clucumi.94@gmail.com>
2022-04-29 18:59:42 -0500

    Created the calculation of the Average

--- average.js
+++ average.js
@@ -1,15 +1,16 @@
-// Element's List
-const list1 = [
-    100,
-    200,
-    300,
-    500,
-];
 
-// For cicle to sum the array
-let sumList1 = 0;
-for(let i=0; i < list1.length; i++){
-    sumList1 = sumList1 + list1[i];
+function caulculateArithmeticMean(list){
+    // For cicle to sum the array
+    // let sumList = 0;
+    // for(let i=0; i < list.length; i++){
+    //     sumList = sumList + list[i];
+    // }
+    const sumList = list.reduce(
+        function (accumulatedValue = 0, newElement){
+            return accumulatedValue + newElement;
+        }
+    );
+
+    const averageList = sumList / list.length;
+    return averageList;
 }
-
-const averageList1 = sumList1 / list1.length;
