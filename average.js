766a970f3b424bbd313d0722175032a7b814dea2
Cesar E Lucumi A <Clucumi.94@gmail.com>
2022-04-29 18:52:22 -0500

    Calculated the average for an specific List

---
+++ average.js
@@ -0,0 +1,15 @@
+// Element's List
+const list1 = [
+    100,
+    200,
+    300,
+    500,
+];
+
+// For cicle to sum the array
+let sumList1 = 0;
+for(let i=0; i < list1.length; i++){
+    sumList1 = sumList1 + list1[i];
+}
+
+const averageList1 = sumList1 / list1.length;
