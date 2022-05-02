058f279c341d5c40756751452412b2cf7475bd71
Cesar E Lucumi A <Clucumi.94@gmail.com>
2022-05-01 20:58:34 -0500

    Calculated the median of the top 10 Colombian Salaries

--- analysis.js
+++ analysis.js
@@ -1,3 +1,36 @@
+//Helpers
+function isEven(number){
+    return (number % 2 === 0);
+}
+
+function caulculateArithmeticMean(list1){
+    const sumList = list1.reduce(
+        function (accumulatedValue = 0, newElement){
+            return accumulatedValue + newElement;
+        }
+    );
+
+    const averageList = sumList / list1.length;
+    return averageList;
+}
+// Median Calculator
+
+function medianSalaries (list){
+    const half = parseInt (list.length/2);
+    if (isEven(list.length)){
+        const personInTheMiddle1 = list[half - 1];
+        const personInTheMiddle2 = list[half];
+
+        const median = caulculateArithmeticMean([personInTheMiddle1,personInTheMiddle2]);
+        return median;
+
+    } else {
+        const personInTheMiddle = list[half];
+        return personInTheMiddle;
+    }
+}
+
+
 // Create a new array only with the Salaries
 const colSalaries = colombia.map(
     function(person){
@@ -14,37 +47,24 @@ const sortedColSalaries = colSalaries.sort(
 );
 
 //Calculate the median
+const generalColMedian = medianSalaries(sortedColSalaries);
 
-function isEven(number){
-    return (number % 2 === 0);
-}
+// top 10% Median
 
-function caulculateArithmeticMean(list1){
-    const sumList = list1.reduce(
-        function (accumulatedValue = 0, newElement){
-            return accumulatedValue + newElement;
-        }
-    );
+// const arrayEg = [0,1,2,3,4,5|||,6,7|||,8,9,10]
+// const EG = arrayEg.splice(5,2);
+// const EG = [6,7]
 
-    const averageList = sumList / list1.length;
-    return averageList;
-}
 
-function medianColSalaries (list){
-    const half = parseInt (list.length/2);
-    if (isEven(list.length)){
-        const personInTheMiddle1 = list[half - 1];
-        const personInTheMiddle2 = list[half];
-
-        const median = caulculateArithmeticMean([personInTheMiddle1,personInTheMiddle2]);
-        return median;
-
-    } else {
-        const personInTheMiddle = list[half];
-        return personInTheMiddle;
-    }
-}
-
-console.log(
-    medianColSalaries(sortedColSalaries)
+const spliceStart = (sortedColSalaries.length * 90)/100;
+const spliceCount = sortedColSalaries.length - spliceStart;
+const salariesColTop10 = sortedColSalaries.splice(
+    spliceStart,
+    spliceCount
 );
+ const top10ColMedian = medianSalaries (salariesColTop10);
+
+console.log({
+    generalColMedian,
+    top10ColMedian,
+});
